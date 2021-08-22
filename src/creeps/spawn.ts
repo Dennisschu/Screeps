706efa2b2fadb31abd4362e69d16a33c62d431b8
Dennisschu <admin@dennis.services>
2021-08-22 12:13:21 +0200

    changed upgrader

--- src/creeps/spawn.ts
+++ src/creeps/spawn.ts
@@ -34,7 +34,7 @@ export default function (spawn: StructureSpawn): void {
       .filter({ memory: { role: `upgrader` } })
       .size() < 6
   ) {
-    const basic: BodyPartConstant[] = [CARRY, CARRY, CARRY, CARRY, MOVE, WORK, WORK];
+    const basic: BodyPartConstant[] = [CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, WORK, WORK];
     spawn.spawnCreep(basic, `upgrader-${uuid()}`, {
       memory: { role: "upgrader", room: "", working: false }
     });
